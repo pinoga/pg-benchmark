fcaf48550cbbc4487b44750291310b5e2d2dd487
Fernando Bizzotto <fbizzotto@blackpointcyber.com>
2026-02-14 16:28:49 +0000

    db utils

---
+++ k6/typings/xk6-sql-postgres/index.d.ts
@@ -0,0 +1,4 @@
+declare module "k6/x/sql/driver/postgres" {
+  const driver: symbol;
+  export default driver;
+}
--- k6/util/database.ts
+++ k6/util/database.ts
@@ -1,2 +1,5 @@
-import sql from 'k6/x/sql'
-import driver from 'k6/x/sql/driver/postgres'
+import sql, { Database } from "k6/x/sql";
+import driver from "k6/x/sql/driver/postgres";
+
+export const connect = (db: string): Database => sql.open(driver, db);
+export const disconnect = (db: Database): void => db.close();
